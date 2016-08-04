90e1711592c30ea7bc5a6911fe4820ccfe569ddc
Barnabas Chan <barny@post.com>
2016-08-04 13:40:42 +0800

    create sessions controller and define methods for users, make main styling and create form for registering

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user,        only: [:show, :edit, :update, :destroy]
+  before_action :logged_in_rockhound, only: [:index, :show, :edit, :update]
+  before_action :correct_rockhound,   only: [:edit, :update]
 
   #GET /users
   def index
@@ -42,5 +44,21 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name, :email, :password,
     :password_confirmation)
   end
+
+  def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in first!"
+        redirect_to login_url
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      unless current_user?(@user) || current_user.admin_rights
+        flash[:danger] = "Don't be lame!"
+        redirect_to(root_url)
+      end
+    end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,12 @@ Rails.application.routes.draw do
   get '/about', to: 'static_pages#about'
   get '/help', to: 'static_pages#help'
 
+  get '/register', to: 'users#new'
+
+  get '/login', to: 'sessions#new'
+  post'/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 
