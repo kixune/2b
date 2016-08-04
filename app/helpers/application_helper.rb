8d72742ad45712be99cea2be20f2db4027d5d6f0
Barnabas Chan <barny@post.com>
2016-08-04 14:57:43 +0800

    touch up header and home page, include sessions helper in application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
   def full_title(page_title = '')
-    base_title = "Rubymart"
+    base_title = "Rubysmart"
     page_title.empty? ? base_title : base_title + " | " +   page_title
   end
 
