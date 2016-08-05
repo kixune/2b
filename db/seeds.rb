7ad75d9b9897657560c56688a73ee5e1dcf811f9
Barnabas Chan <barny@post.com>
2016-08-05 09:30:28 +0800

    minor changes to help page content

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,5 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 User.create(name: "gemperor", email: "king@booboo.com", password: "xxxxxx", password_confirmation: "xxxxxx", admin: true)
+
+User.create(name: "badguy", email: "badguy@people.com", password: "badbad", password_confirmation: "badbad")
