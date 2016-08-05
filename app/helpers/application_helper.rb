c9f446a2ddde3a3a24e44cbec4fc3ee896cd8e0b
Barnabas Chan <barny@post.com>
2016-08-05 10:19:47 +0800

    change page title format

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
   def full_title(page_title = '')
-    base_title = "Rubysmart"
+    base_title = "RubySmart"
     page_title.empty? ? base_title : base_title + " | " +   page_title
   end
 
