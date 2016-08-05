07b8beeca50701d2d11261f6f913af24a38e858d
Barnabas Chan <barny@post.com>
2016-08-05 09:44:01 +0800

    add gemstone before filters

--- app/controllers/gemstones_controller.rb
+++ app/controllers/gemstones_controller.rb
@@ -1,11 +1,28 @@
 class GemstonesController < ApplicationController
+  before_action :set_gemstone, only: [:show,  :update, :destroy]
+  before_action :logged_in_user, only: [:index, :show]
+  before_action :correct_user,   only: [:edit, :update]
+
+
   def index
+    @gemstones = Gemstone.all
   end
 
   def new
+    @gemstones = Gemstone.new
   end
 
   def create
+    @current_user = current_user;
+
+
+    @gemstone =  @current_user.gemstones.create(gemstone_params)
+    if @gemstone.save
+
+      redirect_to @gemstone
+    else
+      render 'new'
+    end
   end
 
   def show
@@ -19,4 +36,21 @@ class GemstonesController < ApplicationController
 
   def destroy
   end
+
+  private
+
+    def set_gemstone
+      @gemstone = Gemstone.find(params[:id])
+    end
+
+    def gemstone_params
+      params.require(:gemstone).permit(:name, :description)
+    end
+
+    def logged_in_user
+      unless logged_in?
+        redirect_to login_url
+      end
+    end
+
 end
